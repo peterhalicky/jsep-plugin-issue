57a29ce5e23ba4662b86d090d71caea371f876e4
Peter Halicky <peter.halicky@myinphinity.com>
2022-08-17 07:22:07 +0200

    initial import of TS project that reproduces the issue

---
+++ index.ts
@@ -0,0 +1,5 @@
+import jsep from 'jsep';
+import comment from '@jsep-plugin/comment';
+import assignment from '@jsep-plugin/assignment';
+jsep.plugins.register(comment);
+jsep.plugins.register(assignment);
